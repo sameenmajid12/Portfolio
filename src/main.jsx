95451716f796500fdaeb523bbe5997656a3c2af3
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-02-05 12:22:04 -0500

    Removed router to simplify and make the website one page

--- src/main.jsx
+++ src/main.jsx
@@ -3,27 +3,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter, createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./Components/Home";
-import Experience from "./Components/Experience";
-import Skills from "./Components/Skills";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        path: "experience",
-        element: <Experience />,
-      },
-      {
-        path: "skills",
-        element: <Skills />,
-      },
-    ],
+    element: <App/>
   },
 ]);
 
