c6323ec4def48a26ecff92e90a3f757b5b079f9e
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-02-05 12:22:42 -0500

    Removed useLocations and useNavigates

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,33 @@ import { useEffect, useState } from "react";
 import Header from "./Components/Header";
 import ContactBar from "./Components/ContactBar";
 import "./index.css";
-import { Outlet } from "react-router-dom";
-
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import Home from "./Components/Home";
+import Projects from "./Components/Projects";
 function App() {
   const [windowTop, setWindowTop] = useState(true);
-  const handleScrollTop = () => {
-    if (window.scrollY === 0) {
-      setWindowTop(true);
-      
-    } else {
-      setWindowTop(false);
-    }
-  };
+  const navLinks = ["", "projects", "experience", "skills"];
+  const location = useLocation();
+  
+
   useEffect(() => {
+    const handleScrollTop = () => {
+      if (window.scrollY === 0) {
+        setWindowTop(true);
+      } else {
+        setWindowTop(false);
+      }
+    };
     window.addEventListener("scroll", handleScrollTop);
     return () => {
       window.removeEventListener("scroll", handleScrollTop);
     };
   });
   return (
-    <div className={`flex-col min-h-screen ${!windowTop?'mt-18':''}`}>
-      <Header windowTop={windowTop}/>
-      <Outlet/>
+    <div className={`flex-col min-h-screen ${!windowTop ? "mt-18" : ""}`}>
+      <Header windowTop={windowTop} />
+      <Home />
+      <Projects/>
       <ContactBar />
     </div>
   );
