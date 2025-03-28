9134f6e08438cd5a8fefbad6bfafb2108da36cc4
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-03-28 01:26:38 -0400

    Stopped scrolling on header menu

--- src/Components/HeaderMenu.jsx
+++ src/Components/HeaderMenu.jsx
@@ -5,8 +5,22 @@ function HeaderMenu({ toggleVisibility, scrollToSection, checkActive}) {
     scrollToSection(route);
     toggleVisibility();
   };
+  useEffect(() => {
+    const preventScroll = (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+    };
+
+    window.addEventListener('wheel', preventScroll, { passive: false });
+    window.addEventListener('touchmove', preventScroll, { passive: false });
+
+    return () => {
+      window.removeEventListener('wheel', preventScroll);
+      window.removeEventListener('touchmove', preventScroll);
+    };
+  }, []);
   return (
-    <div className="flex flex-col gap-y-5 animate-slide-in bg-white w-[100dvw] h-[100dvh] z-200 fixed top-0 bottom-0 right-0 left-0 p-5 pt-[100px]">
+    <div className="flex flex-col gap-y-5 animate-slide-in bg-white w-[100dvw] h-[100dvh] z-200 fixed top-0 bottom-0 right-0 left-0 p-5 pt-[90px] overflow-hidden">
       <div
         className={"flex justify-between p-5  h-18 items-center bg-white border-b-1 border-b-solid border-b-[rgba(0,0,0,0.15)] fixed top-0 right-0 left-0 z-110"}
       >
@@ -22,7 +36,7 @@ function HeaderMenu({ toggleVisibility, scrollToSection, checkActive}) {
         className={`flex items-center gap-x-3 text-[40px] cursor-pointer w-max ${checkActive('home')?'font-bold text-[#313131]':'text-[#474747]'}`}
       >
         Home
-        {checkActive('home')?<i className="fa-solid fa-circle-chevron-left text-xl text-red"></i>:''}
+        {checkActive('home')?<i className="fa-solid fa-circle-chevron-right text-xl text-red"></i>:''}
       </div>
       <div
         onClick={() => navigate("projects")}
